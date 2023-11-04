963f0e8879e53bae38b7ab069f643f52e22b1bdb
aditya saini <adityasaini823@gmail.com>
2023-11-04 23:12:44 +0530

    database connected

--- app/src/main/java/com/example/Quizify/MainActivity.kt
+++ app/src/main/java/com/example/Quizify/MainActivity.kt
@@ -7,14 +7,18 @@ import androidx.activity.compose.setContent
 import androidx.annotation.RequiresApi
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.Quizify.activities.Questionsactivity
 import com.example.Quizify.app.Quizapp
+import com.google.firebase.FirebaseApp
 
 class MainActivity : ComponentActivity() {
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        FirebaseApp.initializeApp(this)
         setContent {
-            Quizapp()
+
+            Questionsactivity()
         }
     }
 }
--- app/src/main/java/com/example/Quizify/activities/Questionsactivity.kt
+++ app/src/main/java/com/example/Quizify/activities/Questionsactivity.kt
@@ -10,27 +10,25 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.selection.selectable
-import androidx.compose.material3.Button
-import androidx.compose.material3.RadioButton
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberUpdatedState
-import androidx.compose.runtime.setValue
+import androidx.compose.material.Button
+import androidx.compose.material.RadioButton
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.Quizify.navigation.Quizapprouter
-import com.example.Quizify.navigation.Screen
 import com.example.Quizify.navigation.SystemBackButtonHandler
+import com.google.firebase.database.*
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import androidx.compose.ui.tooling.preview.Preview
 
+// Define a data class to represent a question
 data class Question(
-    val questionText: String,
-    val options: List<String>
+    val question: String,
+    val answers: List<String>,
+    val correctAnswer: String
 )
 
 @Composable
@@ -46,14 +44,15 @@ fun QuestionItem(question: Question, questionNumber: Int, selectedAnswer: Mutabl
         )
         Spacer(modifier = Modifier.height(8.dp))
         Text(
-            text = question.questionText,
+            text = question.question,
             fontSize = 18.sp
         )
 
         Spacer(modifier = Modifier.height(16.dp))
 
-        val radioOptions = rememberUpdatedState(question.options)
+        val radioOptions = rememberUpdatedState(question.answers)
         val selectedOption = selectedAnswer.value
+        val isCorrect= selectedOption==question.correctAnswer
         radioOptions.value.forEach { option ->
             val isSelected = option == selectedOption
             Row(
@@ -79,6 +78,12 @@ fun QuestionItem(question: Question, questionNumber: Int, selectedAnswer: Mutabl
                 )
                 Spacer(modifier = Modifier.width(8.dp))
                 Text(text = option)
+                if(isSelected && isCorrect){
+                    Text(text = "Correct! ${question.correctAnswer} ", color = Color.Green)
+                }
+                else if (isSelected && !isCorrect) {
+                    Text(text = "Incorrect! ${question.correctAnswer}", color = Color.Red)
+                }
             }
         }
     }
@@ -86,7 +91,7 @@ fun QuestionItem(question: Question, questionNumber: Int, selectedAnswer: Mutabl
 
 @Composable
 fun ListWithButtons(questions: List<Question>) {
-    var currentIndex by remember { mutableIntStateOf(0) }
+    var currentIndex by remember { mutableStateOf(0) }
     val selectedAnswer = remember { mutableStateOf<String?>(null) }
 
     Column(
@@ -94,11 +99,13 @@ fun ListWithButtons(questions: List<Question>) {
             .fillMaxSize()
             .padding(16.dp)
     ) {
-        QuestionItem(
-            question = questions[currentIndex],
-            questionNumber = currentIndex + 1,
-            selectedAnswer = selectedAnswer
-        )
+        if (questions.isNotEmpty()) {
+            QuestionItem(
+                question = questions[currentIndex],
+                questionNumber = currentIndex + 1,
+                selectedAnswer = selectedAnswer
+            )
+        }
 
         Spacer(modifier = Modifier.height(16.dp))
 
@@ -135,21 +142,46 @@ fun ListWithButtons(questions: List<Question>) {
 
 @Composable
 fun Questionsactivity() {
-    val questions = listOf(
-        Question(
-            "What is the capital of Bharat?",
-            listOf("New Delhi", "Mumbai", "Kolkata", "Bangluru")
-        ),
-        Question(
-            "What is the largest planet in our solar system?",
-            listOf("Earth", "Mars", "Jupiter", "Saturn")
-        ),
-        Question(
-            "Who is the president of Bharat?",
-            listOf("Ram Nath Kovind", "Draupadi Murmu", "Narendra Modi", "Subhramanyam Jaishankar")
-        )
-    )
+    // Initialize Firebase database reference
+    val database: DatabaseReference = Firebase.database.reference
+
+    // Fetch questions from Firebase
+    var questions by remember { mutableStateOf(emptyList<Question>()) }
+
+    LaunchedEffect(Unit) {
+        database.child("MCQS").addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val fetchedQuestions = mutableListOf<Question>()
+                for (childSnapshot in snapshot.children) {
+                    val question = childSnapshot.child("question").getValue(String::class.java) ?: ""
+                    val answers = childSnapshot.child("answers").getValue(object : GenericTypeIndicator<List<String>>() {}) ?: emptyList()
+                    val correctAnswer = childSnapshot.child("correct_answers").child("0").getValue(String::class.java) ?: ""
+                    val questionItem = Question(question, answers, correctAnswer)
+                    fetchedQuestions.add(questionItem)
+                }
+                questions = fetchedQuestions
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                // Handle error
+            }
+        })
+    }
 
     ListWithButtons(questions = questions)
     SystemBackButtonHandler()
+}
+@Composable
+@Preview
+fun QuestionsActivityPreview() {
+    val sampleQuestions = listOf(
+        Question(
+            "Sample question 1",
+            listOf("Option 1", "Option 2", "Option 3"),
+            "Option 2"
+        ),
+        // Add more sample questions here
+    )
+
+    ListWithButtons(questions = sampleQuestions)
 }
